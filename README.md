fe67cbe42c71d7bf7daf24befe96d632dbe91166
stormtea123 <1280597658@qq.com>
2015-12-29 14:47:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+##ConvertChineseCharactersFor3
+
+Plugin for Sublime text 3 for convert chinese characters.
+
+![1](https://cloud.githubusercontent.com/assets/1193966/12030060/63ded0e2-ae33-11e5-8f76-b9b2cc34e14e.gif)
+
+![2](https://cloud.githubusercontent.com/assets/1193966/12030061/63e3fac2-ae33-11e5-9a50-78b9ebf2d6e1.gif)
+
+## Features
+
+* Chinese Characters To Unicode
+* Unicode to Chinese Characters
+
+## Install
+
+This package is available through <a href="https://packagecontrol.io/">ConvertChineseCharactersFor3</a>, it’s called “ConvertChineseCharactersfor3”.
+
+###or manually install
+
+cd <Packages directory> (for example on Mac it is ~/Library/Application Support/<Sublime Text Version>/Packages)
+git clone https://github.com/stormtea123/ConvertChineseCharactersFor3.git
+
+## Usage
+
+Select one or more characters and run "convertChineseCharacters" form the context menu. Each character will be replaced.
+
---
+++ convertChineseCharactersFor3.py
@@ -0,0 +1,71 @@
+#-*- coding: utf-8 -*-
+import sublime, sublime_plugin
+import sys
+import re
+
+class ChineseCharactersToUnicodeCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        fileName = self.view.file_name()
+        fileSuffix = re.match(r'.+\.(\w+)$',fileName).group(1)
+        if fileSuffix == "js" or fileSuffix == "css":  
+            def TU(x):
+                if fileSuffix == "js":
+                    return '\\u'+('000'+hex(ord(x))[2:])[-4:]
+                else:
+                    return '\\'+('000'+hex(ord(x))[2:])[-4:]  
+            def toUnicode(x):
+                s = x.group(0)
+                s = list(s)
+                mapValue = map(TU,s)
+                s = "".join(mapValue)
+                return s
+            regions = self.view.sel()
+            if not regions[0].empty():
+                for region in regions:
+                    if not region.empty():
+                        s = self.view.substr(region)
+                        s = re.sub(r"([\u4e00-\u9fa5]+)",toUnicode,s)
+                        self.view.replace(edit, region, s)
+            else:
+                region = sublime.Region(0, self.view.size())
+                s = self.view.substr(region)
+                s = re.sub(r"([\u4e00-\u9fa5]+)",toUnicode,s)
+                
+                self.view.replace(edit, region, s)
+        else:
+            sublime.error_message('不能解析的文件类型')
+            
+class UnicodeToChineseCharactersCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        fileName = self.view.file_name()
+        fileSuffix = re.match(r'.+\.(\w+)$',fileName).group(1)
+        if fileSuffix == "js" or fileSuffix == "css":
+            def unicodeTo(x):
+                s = x.group(0)
+                if fileSuffix == "js":
+                    s = s[2:]
+                else:
+                    s = s[1:]
+                s = chr(int(s,16))
+                return s
+            regions = self.view.sel()
+            if not regions[0].empty():
+                for region in regions:
+                    if not region.empty():
+                        s = self.view.substr(region)
+                        if fileSuffix == "js":
+                            s = re.sub(r"(\\u\w{4})",unicodeTo,s)
+                        else:
+                            s = re.sub(r"(\\\w{4})",unicodeTo,s)
+                        self.view.replace(edit, region, s)
+            else:
+                region = sublime.Region(0, self.view.size())
+                s = self.view.substr(region)
+                if fileSuffix == "js":
+                    s = re.sub(r"(\\u\w{4})",unicodeTo,s)
+                else:
+                    s = re.sub(r"(\\\w{4})",unicodeTo,s)
+                self.view.replace(edit, region, s)
+        else:
+            sublime.error_message('不能解析的文件类型')
