b314455f1cc4d69fc3bb0b972b2822e5fd87d211
thunkli <1280597658@qq.com>
2018-12-07 22:56:24 +0800

    Support for No suffix file with syntax javascript or json or css

--- convertChineseCharactersFor3.py
+++ convertChineseCharactersFor3.py
@@ -1,14 +1,14 @@
-#-*- coding: utf-8 -*-
-import sublime, sublime_plugin
+# -*- coding: utf-8 -*-
+import sublime
+import sublime_plugin
 import sys
 import re
 
 
-def resulveSyntaxType(view):
+def resolveSyntaxType(view):
 
-    # 改用语法类型来判断，这样的话临时文件也能该插件
     syntax = view.settings().get('syntax')
-
+    fileSuffix = ""
     if syntax == "Packages/JavaScript/JSON.sublime-syntax":
         fileSuffix = "json"
     elif syntax == "Packages/JavaScript/JavaScript.sublime-syntax":
@@ -16,6 +16,7 @@ def resulveSyntaxType(view):
     elif syntax == "Packages/CSS/CSS.sublime-syntax":
         fileSuffix = "css"
 
+    print(syntax)
     return fileSuffix
 
 
@@ -23,17 +24,18 @@ class ChineseCharactersToUnicodeCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
 
-        fileSuffix = resulveSyntaxType(self.view)
-        if fileSuffix == "js" or fileSuffix == "json" or fileSuffix == "css":  
+        fileSuffix = resolveSyntaxType(self.view)
+        if fileSuffix == "js" or fileSuffix == "json" or fileSuffix == "css":
             def TU(x):
                 if fileSuffix == "js" or fileSuffix == "json":
                     return '\\u'+('000'+hex(ord(x))[2:])[-4:]
                 else:
-                    return '\\'+('000'+hex(ord(x))[2:])[-4:]  
+                    return '\\'+('000'+hex(ord(x))[2:])[-4:]
+
             def toUnicode(x):
                 s = x.group(0)
                 s = list(s)
-                mapValue = map(TU,s)
+                mapValue = map(TU, s)
                 s = "".join(mapValue)
                 return s
             regions = self.view.sel()
@@ -41,23 +43,22 @@ class ChineseCharactersToUnicodeCommand(sublime_plugin.TextCommand):
                 for region in regions:
                     if not region.empty():
                         s = self.view.substr(region)
-                        s = re.sub(r"([\u4e00-\u9fa5]+)",toUnicode,s)
+                        s = re.sub(r"([\u4e00-\u9fa5]+)", toUnicode, s)
                         self.view.replace(edit, region, s)
             else:
                 region = sublime.Region(0, self.view.size())
                 s = self.view.substr(region)
-                s = re.sub(r"([\u4e00-\u9fa5]+)",toUnicode,s)
-                
+                s = re.sub(r"([\u4e00-\u9fa5]+)", toUnicode, s)
+
                 self.view.replace(edit, region, s)
         else:
-            sublime.error_message('不能解析的文件类型')
-            
+            sublime.error_message('ConvertChineseCharacters allowing you to convert your .js, .json, and .css files')
+
+
 class UnicodeToChineseCharactersCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        # fileName = self.view.file_name()
-        # fileSuffix = re.match(r'.+\.(\w+)$',fileName).group(1)
 
-        fileSuffix = resulveSyntaxType(self.view)
+        fileSuffix = resolveSyntaxType(self.view)
 
         if fileSuffix == "js" or fileSuffix == "json" or fileSuffix == "css":
             def unicodeTo(x):
@@ -66,7 +67,7 @@ class UnicodeToChineseCharactersCommand(sublime_plugin.TextCommand):
                     s = s[2:]
                 else:
                     s = s[1:]
-                s = chr(int(s,16))
+                s = chr(int(s, 16))
                 return s
             regions = self.view.sel()
             if not regions[0].empty():
@@ -74,17 +75,17 @@ class UnicodeToChineseCharactersCommand(sublime_plugin.TextCommand):
                     if not region.empty():
                         s = self.view.substr(region)
                         if fileSuffix == "js" or fileSuffix == "json":
-                            s = re.sub(r"(\\[uU]\w{4})",unicodeTo,s)
+                            s = re.sub(r"(\\[uU]\w{4})", unicodeTo, s)
                         else:
-                            s = re.sub(r"(\\\w{4})",unicodeTo,s)
+                            s = re.sub(r"(\\\w{4})", unicodeTo, s)
                         self.view.replace(edit, region, s)
             else:
                 region = sublime.Region(0, self.view.size())
                 s = self.view.substr(region)
                 if fileSuffix == "js" or fileSuffix == "json":
-                    s = re.sub(r"(\\[uU]\w{4})",unicodeTo,s)
+                    s = re.sub(r"(\\[uU]\w{4})", unicodeTo, s)
                 else:
-                    s = re.sub(r"(\\\w{4})",unicodeTo,s)
+                    s = re.sub(r"(\\\w{4})", unicodeTo, s)
                 self.view.replace(edit, region, s)
         else:
-            sublime.error_message('不能解析的文件类型')
+            sublime.error_message('ConvertChineseCharacters allowing you to convert your .js, .json, and .css files')
